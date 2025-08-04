88db5d9eedb38906cc1ed706cff63260e3f4e4a1
2630548 <130237209+2630548@users.noreply.github.com>
2025-08-04 10:12:09 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -588,7 +588,7 @@ function PlayPageClient() {
         this.queue = [];
     this.activeRequests = 0;
     this.maxConcurrent = 6; // 自定义最大并发请求数
-customload(context, config, callbacks) {
+function customload(context, config, callbacks) {
     if (this.activeRequests < this.maxConcurrent) {
       this.activeRequests++;
       // 使用 Fetch API 或 XMLHttpRequest 加载分片
@@ -609,7 +609,7 @@ customload(context, config, callbacks) {
     }
   }
 
-  processQueue() {
+  function processQueue() {
     if (this.queue.length && this.activeRequests < this.maxConcurrent) {
       const { context, config, callbacks } = this.queue.shift();
       customload(context, config, callbacks);
