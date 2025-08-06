c96f9379c8cff478f870e3695615b8de43a17053
2630548 <130237209+2630548@users.noreply.github.com>
2025-08-06 20:30:54 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1280,10 +1280,10 @@ function PlayPageClient() {
               lowLatencyMode: true, // 开启低延迟 LL-HLS
 
               /* 缓冲/内存相关 */
-              maxBufferLength: 60, // 前向缓冲最大 30s，过大容易导致高延迟
-              maxMaxBufferLength: 600,
-              backBufferLength: 60, // 仅保留 30s 已播放内容，避免内存占用
-              maxBufferSize: 200 * 1000 * 1000, // 约 60MB，超出后触发清理
+              maxBufferLength: 100, // 前向缓冲最大 30s，过大容易导致高延迟
+              maxMaxBufferLength: 1000,
+              backBufferLength: 100, // 仅保留 30s 已播放内容，避免内存占用
+              maxBufferSize: 300 * 1000 * 1000, // 约 60MB，超出后触发清理
               // maxConcurrentRequests: 6,    // 最大并发请求数
 
               /* 自定义loader */
